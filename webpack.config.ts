e5de05c8bff2be05d202b9d40f0d7f95931ff582
Bekzat Abdibakas <babdibakas@yandex.kz>
2025-06-17 08:12:45 +0500

    feat: installed mini-css-extract-plugin, added Dockerfile, added production profile for docker-compose

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,44 +2,59 @@ import webpack from "webpack"
 import path from "path"
 import webpackDevServer from "webpack-dev-server"
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-const config: webpack.Configuration = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash:8].js',
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
-                ],
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 3000,
-    }
+interface IEnv {
+    mode: "development" | "production"
 }
 
-export default config
+export default (env: IEnv) => {
+    const mode = env.mode || "development"
+    const isDev = mode === "development"
+
+    const config: webpack.Configuration = {
+        mode,
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash:8].js',
+            clean: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader",
+                    ],
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html')
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css',
+                chunkFilename: '[id].[contenthash].css',
+            })
+        ],
+        devtool: isDev ?  'inline-source-map' : undefined,
+        devServer: isDev ? {
+            port: 3000,
+        } : undefined
+    }
+
+    return config
+}
