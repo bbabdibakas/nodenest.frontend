b9f8122e74cc13c523dc7e0a0cd4c5ce98d8fb07
Bekzat Abdibakas <babdibakas@yandex.kz>
2025-06-17 08:45:50 +0500

    feat: setup react css modules, added ui kit AppInput

---
+++ src/components/AppInput/AppInput.tsx
@@ -0,0 +1,40 @@
+import {ChangeEvent, InputHTMLAttributes} from 'react';
+import * as styles from './AppInput.module.scss';
+
+interface AppInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
+    className?: string
+    value: string
+    placeholder: string
+    onChange: (value: string) => void
+}
+
+export const AppInput = (props: AppInputProps) => {
+    const {
+        className,
+        value,
+        onChange,
+        placeholder,
+        maxLength = 32,
+        ...otherProps
+    } = props
+
+    const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value)
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <input
+                className={`${styles.AppInput} ${className}`}
+                value={value}
+                onChange={onChangeValue}
+                placeholder={placeholder}
+                maxLength={maxLength}
+                {...otherProps}
+            />
+            <div className={styles.placeholder}>
+                {placeholder}
+            </div>
+        </div>
+    )
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,16 @@
+import {AppInput} from "../components/AppInput/AppInput";
+import {useState} from "react";
+
 const MainPage = () => {
+    const [value, setValue] = useState('')
+
+    const onChangeValue = (value: string) => {
+        setValue(value)
+    }
+
     return (
         <div className="page">
-            Main Page
+            <AppInput value={value} onChange={onChangeValue} placeholder={'Username'}/>
         </div>
     )
 }
